88b0f859300a1a8008ce002710605fa6d0388e36
Carwin Young <carwin.young@snyk.io>
2022-08-19 12:26:56 -0500

    Commit globbed index exports.

--- src/lib/controllers/index.ts
+++ src/lib/controllers/index.ts
@@ -1,9 +1,2 @@
-import fs from 'fs';
-import path from 'path';
-
-const curdir = path.dirname;
-console.log(curdir);
-const fullPath = path.join(__dirname, 'target_dir')
-console.log(fullPath);
-
-fs.readdir
+export * from './snykAuth'
+export * from './snykAuthCallback'
